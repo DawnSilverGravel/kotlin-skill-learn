9289cc925000c09429c2db51e89ab4043ae0389d
DawnSilverGravel <xxx15767377519@163.com>
2024-04-17 22:28:33 +0800

    feat: 添加基础编程题目 25.题目：请输入星期几的第一个字母来判断一下是星期几 26.题目：对10个数进行排序 27.题目：求一个3*3矩阵对角线元素之和

---
+++ basic/src/main/kotlin/com/silvergravel/basic/NumberSort.kt
@@ -0,0 +1,45 @@
+package com.silvergravel.basic
+
+import kotlin.random.Random
+
+/**
+ * 题目：对10个数进行排序
+ * @author DawnStar
+ * @since : 2024/4/17
+ */
+class NumberSort {
+    fun sortByMethod(list: List<Int>): List<Int> {
+        return list.sortedByDescending { it }.toList()
+    }
+
+    fun sortByBubble(list: List<Int>): List<Int> {
+        val array = list.toIntArray()
+        val size = array.size
+        for (i in 0 until size) {
+            for (j in 0 until size - i - 1) {
+                if (array[j + 1] > array[j]) {
+                    array[j + 1] = array[j].also {
+                        array[j] = array[j + 1]
+                    }
+                }
+            }
+        }
+        return array.toList()
+    }
+}
+
+fun main() {
+    val random = Random(3000)
+    val count = random.nextInt(5, 20)
+    val list = arrayListOf<Int>().apply {
+        for (i in 0..count) {
+            add(random.nextInt(1, 200))
+        }
+    }
+
+    println("原数组：$list")
+    val numberSort = NumberSort()
+
+    println("排序后数组：${numberSort.sortByMethod(list)}")
+    println("排序后数组：${numberSort.sortByBubble(list)}")
+}
---
+++ basic/src/main/kotlin/com/silvergravel/basic/WeekJudge.kt
@@ -0,0 +1,42 @@
+package com.silvergravel.basic
+
+import java.util.*
+
+/**
+ * 题目：请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续 判断第二个字母
+ * @author DawnStar
+ * @since : 2024/4/17
+ */
+class WeekJudge {
+    fun judgeWeek(prefixWord: String): WeeKEnum {
+        for (value in WeeKEnum.values()) {
+            if (value.name.startsWith(prefixWord.uppercase())) {
+                return value
+            }
+        }
+        throw IllegalArgumentException("非法参数")
+    }
+}
+
+// 必须加`val`才可以访问
+enum class WeeKEnum(val description: String) {
+
+    MONDAY("周一"),
+    TUESDAY("周二"),
+    WEDNESDAY("周三"),
+    THURSDAY("周四"),
+    FRIDAY("周五"),
+    SATURDAY("周六"),
+    SUNDAY("周日");
+
+}
+
+fun main() {
+    // in 是 kotlin中的硬关键字
+    val scanner = Scanner(System.`in`)
+    print("输入星期字母前缀，(顺序匹配)：")
+    val dayOfWeek = scanner.nextLine()
+    val judgeWeek = WeekJudge().judgeWeek(dayOfWeek)
+    println(judgeWeek.description)
+}
+
